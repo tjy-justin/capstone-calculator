2c56b2dfdd2e664bb97dc4463af371514b1c118c
Tan Jin Yi <rayneedust@gmail.com>
2022-06-11 17:18:06 +0800

    Implement basic operators

---
+++ script.js
@@ -0,0 +1,27 @@
+let a = 69;
+let b = 14;
+
+function add(a, b) {
+  console.log(a + b);
+  subtract(a, b);
+  multiply(a, b);
+  divide(a, b);
+}
+
+function subtract(a, b) {
+  console.log(a - b);
+}
+
+function multiply(a, b) {
+  console.log(a * b);
+}
+
+function divide(a, b) {
+  console.log(a / b);
+}
+
+// function power(a, b) {
+//   console.log(a ** b);
+// }
+
+add(a, b);
