24d4188f5b0a12a2d9f4b7ef637621dd3efd3219
Tan Jin Yi <rayneedust@gmail.com>
2022-06-17 17:33:48 +0800

    Significantly improve JS logic
    - Add decimals, negative
    - Clear and delete chars
    - Add multiple rounds
    - Warn of zero division and char limit
    - Implement font scaling

--- README.md
+++ README.md
@@ -12,18 +12,19 @@ This project is hosted on **GitHub Pages**. Feel free to [:link: try it out](htt
 - [ ] Multiple ops in a string
 - [ ] BODMAS logic
 - [ ] Round results with long decimals
-- [ ] Implement decimals
-- [ ] Implement negative numbers
+- [x] Implement decimals
+- [x] Implement negative numbers
 - [ ] Implement keyboard shortcuts
-- [ ] Clear entire calculation
-- [ ] Delete characters
-- [ ] Warn of zero division
+- [x] Clear entire calculation
+- [x] Delete characters
+- [x] Warn of zero division
 - [ ] Evaluate only a single pair of numbers at a time
-- [ ] Operators won't register w/o numbers or when entered twice
-- [ ] Multiple round of operation when equalBtn pressed repeatedly
-- [ ] CSS: opsBtn diff. color from numBtn
-- [ ] CSS: Implement Grid
-- [ ] CSS: Implement modern-normalize
+- [x] Operators won't register w/o numbers or when entered twice
+- [x] Multiple round of operation when equalBtn pressed repeatedly
+- [x] CSS: Implement display font scaling
+- [x] CSS: opsBtn diff. color from numBtn
+- [x] CSS: Implement Grid
+- [x] CSS: Implement modern-normalize
 
 ## Resources
 
@@ -32,5 +33,7 @@ Below are a collection of resources which helped improve my skills :sparkles: wh
 - [if-else vs switch - javatpoint](https://www.javatpoint.com/if-else-vs-switch)
 - [What properties can I use with event.target? - Stack Overflow](https://stackoverflow.com/questions/7723188/what-properties-can-i-use-with-event-target)
 - [javascript - ReferenceError: Cannot access 'variable' before initialization - Stack Overflow](https://stackoverflow.com/questions/66976912/referenceerror-cannot-access-variable-before-initialization)
+- [html - How to set min-font-size in CSS - Stack Overflow](https://stackoverflow.com/questions/23984629/how-to-set-min-font-size-in-css)
+- [css - Font scaling based on width of container - Stack Overflow](https://stackoverflow.com/questions/16056591/font-scaling-based-on-width-of-container)
 
 Built with :heart: to **The Odin Project**, a wonderful Web Development curriculum. ~
--- script.js
+++ script.js
@@ -1,27 +1,120 @@
-let a = 69;
-let b = 14;
+// Functions with initialization priority
 
-function add(a, b) {
-  console.log(a + b);
-  subtract(a, b);
-  multiply(a, b);
-  divide(a, b);
+function evaluate() {
+  if (operator && round == 1) {
+    lastCalc.textContent = lastCalc.textContent.concat(
+      " ",
+      currentCalc.textContent
+    );
+    secondOperand = currentCalc.textContent;
+    currentCalc.textContent = operate(operator, firstOperand, secondOperand);
+    round++;
+  } else {
+    lastCalc.textContent = `${currentCalc.textContent} ${operator} ${secondOperand}`;
+    firstOperand = currentCalc.textContent;
+    currentCalc.textContent = operate(operator, firstOperand, secondOperand);
+  }
 }
 
-function subtract(a, b) {
-  console.log(a - b);
+function clearCalc() {
+  currentCalc.textContent = "";
+  lastCalc.textContent = "";
+  operator = "";
+  firstOperand = "";
+  secondOperand = "";
+  round = 1;
 }
 
-function multiply(a, b) {
-  console.log(a * b);
+function addDecimal() {
+  if (
+    !currentCalc.textContent.includes(".") &&
+    currentCalc.textContent.length <= 10
+  ) {
+    currentCalc.textContent = currentCalc.textContent.concat("", ".");
+  }
 }
 
-function divide(a, b) {
-  console.log(a / b);
+// Listeners
+
+let operator;
+let firstOperand;
+let secondOperand;
+let round = 1;
+let result;
+
+let lastCalc = document.querySelector(".lastCalc");
+let currentCalc = document.querySelector(".currentCalc");
+let clearBtn = document.querySelector(".clearBtn");
+let negBtn = document.querySelector(".negBtn");
+let delBtn = document.querySelector(".delBtn");
+let numBtn = document.querySelectorAll(".numBtn");
+let opsBtn = document.querySelectorAll(".opsBtn");
+let equalBtn = document.querySelector(".equalBtn");
+let decimalBtn = document.querySelector(".decimalBtn");
+
+clearBtn.addEventListener("click", clearCalc);
+delBtn.addEventListener("click", deleteNumber);
+negBtn.addEventListener("click", setNegative);
+
+numBtn.forEach((button) =>
+  button.addEventListener("click", () => setNumber(button.textContent))
+);
+opsBtn.forEach((button) =>
+  button.addEventListener("click", () => setOperator(button.textContent))
+);
+
+equalBtn.addEventListener("click", evaluate);
+decimalBtn.addEventListener("click", addDecimal);
+
+// Functions
+
+function operate(operator, firstOperand, secondOperand) {
+  let a = parseFloat(firstOperand);
+  let b = parseFloat(secondOperand);
+
+  if (operator === "+") {
+    result = a + b;
+  } else if (operator === "-") {
+    result = a - b;
+  } else if (operator === "×") {
+    result = a * b;
+  } else if (operator === "÷") {
+    result = a / b;
+  }
+
+  if (result.toString().length < 12) {
+    return result;
+  } else {
+    return result.toExponential(5);
+  }
 }
 
-// function power(a, b) {
-//   console.log(a ** b);
-// }
+function setNumber(numBtn) {
+  let input = currentCalc.textContent;
 
-add(a, b);
+  if (input.length >= 10) {
+    alert("Max characters entered.");
+  } else if (input.length == 0 && operator === "÷" && numBtn === "0") {
+    alert("Ha, as intriguing as it sounds, you can't divide by a zero!");
+  } else {
+    currentCalc.textContent = input.concat("", numBtn);
+  }
+}
+
+function setOperator(opsBtn) {
+  if (currentCalc.textContent) {
+    round = 1;
+    operator = opsBtn;
+    firstOperand = currentCalc.textContent;
+    lastCalc.textContent = currentCalc.textContent.concat(" ", operator);
+    currentCalc.textContent = "";
+  }
+}
+
+function deleteNumber() {
+  currentCalc.textContent = currentCalc.textContent.toString().slice(0, -1);
+}
+
+function setNegative() {
+  currentCalc.textContent = (currentCalc.textContent * -1).toString();
+}
